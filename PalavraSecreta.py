5ab97844d29b413d231aeb764975990745bd2672
nclhenri <nicolashenrisilva@outlook.com>
2026-03-19 12:32:53 -0300

    Reescrito pela 5 vez

--- PalavraSecreta.py
+++ PalavraSecreta.py
@@ -1,30 +1,36 @@
-palavra_secreta = "SãoPauloTriMundial"
-letras_acertadas = ""
+palavra_secreta = "Nicolas"
 tentativas = 0
-print("Teste a sua sorte!")
+letras_acertadas = ""
+print("Teste a sua sorte.")
 tentativa = input("Digite uma letra:")
 
 while tentativa != palavra_secreta:
 
+    if len(tentativa) > 1:
+        print("Você digitou mais de uma letra!")
+        print("Tente novamente.")
+        tentativas += 1
+        tentativa = input("Digite uma letra:")
+        continue
+    
     if tentativa in palavra_secreta:
         print("Essa letra TEM na palavra secreta!")
-        tentativas += 1
         letras_acertadas += tentativa
+        tentativas += 1
     else:
         print("Essa letra NÃO TEM na palavra secreta!")
+        tentativas += 1
 
-    palavra_formada = ""
+    palavra_formada = " "
 
     for letra_secreta in palavra_secreta:
         if letra_secreta in letras_acertadas:
             palavra_formada += letra_secreta
         else:
             palavra_formada += "*"
-
     print(f"Palavra gerada: {palavra_formada}")
-    tentativa = input("Digite outra letra:")
-    tentativas += 1
-print("PARABÉNS!!! Você acertou!")
-print(f"A palavra secreta é: {palavra_secreta}")
-print(f"Tentativas necessárias: {tentativas}")
+    tentativa = input("Tente outra letra:")
+print(f"PARABÉNS! Você acertou em {tentativas} tentativas.")
+print(f"A palavra secreta era: {palavra_secreta}")
+
     
